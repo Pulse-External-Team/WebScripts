5a26202975c58f44e36a6025bac18fc6fe311671
Pulse External Team <jqgsmfang@gmail.com>
2025-03-16 00:46:44 +0100

    Roblox Blade Ball.js

--- WebScripts/Scratch/Loader/Roblox Blade Ball.js
+++ WebScripts/Scratch/Loader/Roblox Blade Ball.js
@@ -1,3 +1,3 @@
 // Game: https://scratch.mit.edu/projects/930523177/
 
-eval(await (await fetch('')).text());
+eval(await (await fetch('https://raw.githubusercontent.com/Pulse-External-Team/WebScripts/refs/heads/main/WebScripts/Scratch/Scripts/Roblox%20Blade%20Ball.js')).text());
