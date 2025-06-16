69775a6a678f527ce4e3f331224b537f3dfd0ce9
Pulse External <jqgsmfang@gmail.com>
2025-06-16 15:09:50 +0200

    Update example.js

--- PulseNotify/example.js
+++ PulseNotify/example.js
@@ -1,4 +1,9 @@
+// Load the PulseNotify API
+fetch("https://raw.githubusercontent.com/Pulse-External-Team/WebScripts/refs/heads/main/PulseNotify/main.js").then(r => r.text()).then(code => (new Function(code))());
+
+// Sending Notifycation:
+//                    Titel            Message
 PulseNotify.Notify("Injected", "PulseJS is injected", {
-  type: "success",
-  duration: 3000
+  type: "success", // success, info, warning, error
+  duration: 3000 // Duration in milliseconds
 });
